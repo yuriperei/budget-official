d7128a0b0f4252d1b4288448e4fce978df3e9327
Calebe Santos <calebe.santos@Calebes-iMac.local>
2016-03-30 19:58:02 -0700

    Alter changes

--- Budget/ViewController.swift
+++ Budget/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     //Added comment
+    //Added other comentary
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
